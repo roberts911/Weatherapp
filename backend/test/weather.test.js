e928c16725d6c1b37157da25022514cc288ebfc8
Robert Siurek <85109223+roberts911@users.noreply.github.com>
2023-06-19 16:23:23 +0200

    Added comments

--- backend/test/weather.test.js
+++ backend/test/weather.test.js
@@ -1,18 +1,25 @@
+// Import required modules for testing
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { describe, it } = require('mocha');
 const { expect } = chai;
+
+// Use chaiHttp for making HTTP requests in tests
 chai.use(chaiHttp);
 
+// Test suite for '/api/weather' endpoint
 describe('GET /api/weather', () => {
+  // Test case: It should return weather data
   it('should return weather data', (done) => {
+     // Make a GET request to the server
     chai
       .request('http://localhost:9000')
       .get('/api/weather?lat=60.17&lon=24.94')
       .end((err, res) => {
         if (err) {
-          done(err); // Dodaj obsługę błędu
+          done(err); // Handle any error that occurred during the request
         } else {
+          // Expectations for the server response
           expect(res).to.have.status(200);
           expect(res.body).to.be.an('object');
           expect(res.body).to.have.property('id');
@@ -24,24 +31,28 @@ describe('GET /api/weather', () => {
           expect(res.body).to.have.property('feels_like');
           expect(res.body).to.have.property('humidity');
           expect(res.body).to.have.property('windSpeed');
-          done();
+          done(); // Signal that the test case is done
         }
       });
   });
 });
 
+// Test suite for '/api/forecast' endpoint
 describe('GET /api/forecast', () => {
+  // Test case: It should return forecast data
   it('should return forecast data', (done) => {
+    // Make a GET request to the server
     chai
       .request('http://localhost:9000')
       .get('/api/forecast?lat=60.17&lon=24.94')
       .end((err, res) => {
         if (err) {
-          done(err); // Dodaj obsługę błędu
+          done(err); // Handle any error that occurred during the request
         } else {
+          // Expectations for the server response
           expect(res).to.have.status(200);
           expect(res.body).to.be.an('array');
-          done();
+          done(); // Signal that the test case is done
         }
       });
   });
